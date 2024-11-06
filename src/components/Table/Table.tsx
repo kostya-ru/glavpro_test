6c53b5f4629cc973cfd455a1d6646b277677cd58
kostya-ru <kostya-ru@list.ru>
2024-11-06 22:27:18 +0300

    Table component, classNames, title

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import type * as T from "./Table.types";
+import classNames from "classnames";
+import styles from "./Table.module.css";
+
+const Table: FC<T.TableProps> = (props) => {
+  const { className } = props;
+  const rootClasses = classNames(styles.root, className);
+  return <h1 className={rootClasses}>OLOLO</h1>;
+};
+
+export default Table;
---
+++ src/components/Table/index.ts
@@ -0,0 +1,2 @@
+export { default as Table } from "./Table";
+export type * as T from "./Table.types";
